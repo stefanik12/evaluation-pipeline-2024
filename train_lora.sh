e182bde0167ac52eebf1e6be4945b8e919843e6c
Aaron Mueller <ammaaronmueller@gmail.com>
2024-05-03 23:02:04 -0400

    remove commented out code in LoRA script

--- train_lora.sh
+++ train_lora.sh
@@ -3,7 +3,6 @@
 MODELPATH=$1
 
 for task in {boolq,cola,mnli,mrpc,multirc,qnli,qqp,rte,sst2,wsc}; do
-# for task in {qqp,rte,sst2,wsc}; do
     python train_lora.py \
         $MODELPATH \
         $task \
